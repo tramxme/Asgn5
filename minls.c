f7f3be9ea6d0b4c8292e104b61e63221acfe73df
tramxme <tramlaisf@gmail.com>
2016-03-10 14:43:34 -0800

    My change

--- minls.c
+++ minls.c
@@ -7,7 +7,7 @@
 #include "minfs.h"
 
 /* This function checks if a partition Table is valid */
-int validPT(FILE *image, uint16_t offset){
+int validPT(FILE *image, uint32_t offset){
    uint8_t b510, b511;
    int res;
 
@@ -39,7 +39,7 @@ void printSuperblock(superblock *sp){
    uint16_t zonesize = sp->blocksize << sp->log_zone_size;
    printf("Stored Fields:\n");
    printf("  %-*s %*u\n", width, "ninodes", width, sp->ninodes);
-   printf("  %-*s %*d\n", width, "i_blocks", width, sp->i_blocks); 
+   printf("  %-*s %*d\n", width, "i_blocks", width, sp->i_blocks);
    printf("  %-*s %*d\n", width, "z_blocks", width, sp->z_blocks);
    printf("  %-*s %*d\n", width, "firstdata", width, sp->firstdata);
    printf("  %-*s %*d, (zone size: %d)\n", width, "log_zone_size",
@@ -120,6 +120,9 @@ void printInode(inode *fInode){
          free(perm);
 }
 
+/* This functions check if a filename is in the directory
+ * It return -1 if the filename does not exist, otherwise return inode number */
+
 void printFiles(FILE *in, superblock *sb, int offset, char *path,
  inode *fInode, uint16_t zonesize){
    inode *currInode;
@@ -127,201 +130,212 @@ void printFiles(FILE *in, superblock *sb, int offset, char *path,
 
    printf("%s:\n", path);
 
-   dir_entry *dirEntry = calloc(sizeof(dir_entry), fInode->size/DIR_ENTRY_SIZE);
+      dir_entry *dirEntry = calloc(sizeof(dir_entry), fInode->size/DIR_ENTRY_SIZE);
 
-   /* Root directory */
-   fseek(in, offset + fInode->zone[0] * zonesize, SEEK_SET);
-   fread(dirEntry, sizeof(dir_entry), fInode->size/DIR_ENTRY_SIZE, in);
-
-   for(i = 0; i < fInode->size/DIR_ENTRY_SIZE; i++){
-      if (dirEntry[i].inode != 0){
-         //printf("inode number %u - filename %s\n", dirEntry[i].inode, dirEntry[i].name);
-         currInode = getInode(in, offset, sb, dirEntry[i].inode);
-	 printf("%s %*d %s\n", getPerm(currInode->mode), FORMAT_WIDTH,
-	  currInode->size, dirEntry[i].name); 
-      }
-   }
-   free(dirEntry);
-   free(currInode);
-}
-
-int main(int argc, char **argv){
-   int v = 0, h = 0, p = 0, s = 0;
-   int part = -1, sub = -1;
-   char imagefile[MAX_LENGTH], path[MAX_LENGTH];
-   char *ptr;
-   int c, res;
-   FILE *image;
-   char *usage = "usage: minls   [ -v ] [ -p num ] ] imagefile [ path ]\n"
-      "Options:\n"
-      "-p   part    --- select partition for filesystem (default: none)\n"
-      "-s   sub     --- select subpartition for filesystem (default: none)\n"
-      "-h   help    --- print usage information and exit\n"
-      "-v   verbose --- increase verbosity level";
-
-   pt_entry *pt = calloc(sizeof(pt_entry), 4);
-   pt_entry *subpt = calloc(sizeof(pt_entry), 4);
-   superblock *sBlock = calloc(sizeof(superblock), 1);
-   inode *Inode = calloc(sizeof(inode), 1);
-   uint16_t zonesize;
-   uint32_t offset = 0;
-
-   //TODO: error catching when necessary info is not provided
-   if (argc == 1){
-      printf("%s\n", usage);
-      return EXIT_FAILURE;
-   }
-   else {
-      while ((c = getopt(argc, argv, "vhp:s:")) > 0){
-         switch (c){
-            case 'v':
-               v = 1;
-               break;
-            case 'h':
-               h = 1;
-               printf("%s\n", usage);
-               return EXIT_FAILURE;
-            case 'p':
-               p = 1;
-               part = strtol(optarg, &ptr, 10);
-               if (part >= MAX_PART){
-                  perror("Cannot have more than 4 partitions");
-                  return EXIT_FAILURE;
-               }
-               break;
-            case 's':
-               s = 1;
-               sub =  strtol(optarg, &ptr, 10);
-               if (part < 0 && sub >= 0){
-                  perror("You cannot have subpartition without partition");
-                  return EXIT_FAILURE;
-               }
-
-               if (sub >= MAX_PART){
-                  perror("Cannot have more than 4 partitions");
-                  return EXIT_FAILURE;
-               }
-               break;
-         }
-      }
-
-      strcpy(imagefile, argv[optind]); /* Get imagefile */
-
-      /* Get the path if provided */
-      if (++optind < argc){
-         strcpy(path, argv[optind]);
-      }
-   }
-
-   image = fopen(imagefile, "r");
-
-   /* Partitioned image */
-   if (p){
-      if (!validPT(image, BYTE510)){
-         perror("Not valid parition table - subpartition");
-         return EXIT_FAILURE;
-      }
-      if ((res = fseek(image, PART_OFFSET, SEEK_SET)) < 0){
+      /* Root directory */
+      if ((res = fseek(in, offset + fInode->zone[0] * zonesize, SEEK_SET)) < 0){
          perror("fseek failed");
          return EXIT_FAILURE;
       }
 
-      fread(pt, sizeof(pt_entry), 4, image);
-
-      /* Validate MINIX parition */
-      if (pt[part].type != MINIX_PART){
-         perror("This is not a MINIX partition\n");
+      if ((res = fread(dirEntry, sizeof(dir_entry), fInode->size/DIR_ENTRY_SIZE, in)) < 0){
+         perror("fread failed");
          return EXIT_FAILURE;
       }
 
-      offset = pt[part].lFirst * SECTOR_SIZE;
+      for(i = 0; i < fInode->size/DIR_ENTRY_SIZE; i++){
+         if (dirEntry[i].inode != 0){
+            //printf("inode number %u - filename %s\n", dirEntry[i].inode, dirEntry[i].name);
+            currInode = getInode(in, offset, sb, dirEntry[i].inode);
+      printf("%s %*d %s\n", getPerm(currInode->mode), FORMAT_WIDTH,
+       currInode->size, dirEntry[i].name);
+         }
+      }
+      free(dirEntry);
+      free(currInode);
+   }
 
-      /* If subpartition is provided */
-      if (s){
-         /* Validate the subpartition table */
-         if (!validPT(image, pt[part].lFirst * SECTOR_SIZE + BYTE510)){
+   int main(int argc, char **argv){
+      int v = 0, p = 0, s = 0;
+      int part = -1, sub = -1;
+      char imagefile[MAX_LENGTH], path[MAX_LENGTH];
+      char *ptr;
+      int c, res;
+      FILE *image;
+      char *usage = "usage: minls   [ -v ] [ -p num ] ] imagefile [ path ]\n"
+         "Options:\n"
+         "-p   part    --- select partition for filesystem (default: none)\n"
+         "-s   sub     --- select subpartition for filesystem (default: none)\n"
+         "-h   help    --- print usage information and exit\n"
+         "-v   verbose --- increase verbosity level";
+
+      pt_entry *pt = calloc(sizeof(pt_entry), 4);
+      pt_entry *subpt = calloc(sizeof(pt_entry), 4);
+      superblock *sBlock = calloc(sizeof(superblock), 1);
+      inode *Inode = calloc(sizeof(inode), 1);
+      uint16_t zonesize;
+      uint32_t offset = 0;
+
+      //TODO: error catching when necessary info is not provided
+      if (argc == 1){
+         printf("%s\n", usage);
+         return EXIT_FAILURE;
+      }
+      else {
+         while ((c = getopt(argc, argv, "vhp:s:")) > 0){
+            switch (c){
+               case 'v':
+                  v = 1;
+                  break;
+               case 'h':
+                  printf("%s\n", usage);
+                  return EXIT_FAILURE;
+               case 'p':
+                  p = 1;
+                  part = strtol(optarg, &ptr, 10);
+                  if (part >= MAX_PART){
+                     perror("Cannot have more than 4 partitions");
+                     return EXIT_FAILURE;
+                  }
+                  break;
+               case 's':
+                  s = 1;
+                  sub =  strtol(optarg, &ptr, 10);
+                  if (part < 0 && sub >= 0){
+                     perror("You cannot have subpartition without partition");
+                     return EXIT_FAILURE;
+                  }
+
+                  if (sub >= MAX_PART){
+                     perror("Cannot have more than 4 partitions");
+                     return EXIT_FAILURE;
+                  }
+                  break;
+            }
+         }
+
+         strcpy(imagefile, argv[optind]); /* Get imagefile */
+
+         /* Get the path if provided */
+         if (++optind < argc){
+            strcpy(path, argv[optind]);
+         }
+      }
+
+      image = fopen(imagefile, "r");
+
+      /* Partitioned image */
+      if (p){
+         if (!validPT(image, BYTE510)){
             perror("Not valid parition table - subpartition");
             return EXIT_FAILURE;
          }
-         /* Get the subpartition table */
-         if ((res = fseek(image, PART_OFFSET + pt[part].lFirst * SECTOR_SIZE,
-                     SEEK_SET)) < 0){
+
+         if ((res = fseek(image, PART_OFFSET, SEEK_SET)) < 0){
             perror("fseek failed");
             return EXIT_FAILURE;
          }
 
-         fread(subpt, sizeof(pt_entry), 4, image);
+         if ((res = fread(pt, sizeof(pt_entry), 4, image)) < 0){
+            perror("fread failed");
+            return EXIT_FAILURE;
+         }
 
-         /* Validate MINIX subparition */
-         if (subpt[sub].type != MINIX_PART){
+         /* Validate MINIX parition */
+         if (pt[part].type != MINIX_PART){
             perror("This is not a MINIX partition\n");
             return EXIT_FAILURE;
          }
 
-         /* Get the file system */
-         offset = subpt[sub].lFirst * SECTOR_SIZE;
-      }
-      else{
-         /* Get the file system */
-         if (( res = fseek(image, PART_OFFSET + pt[part].lFirst * SECTOR_SIZE
-                     , SEEK_SET)) < 0){
-            perror("fseek failed");
-            return EXIT_FAILURE;
+         offset = pt[part].lFirst * SECTOR_SIZE;
+
+         /* If subpartition is provided */
+         if (s){
+            /* Validate the subpartition table */
+            if (!validPT(image, pt[part].lFirst * SECTOR_SIZE + BYTE510)){
+               perror("Not valid parition table - subpartition");
+               return EXIT_FAILURE;
+            }
+            /* Get the subpartition table */
+            if ((res = fseek(image, PART_OFFSET + pt[part].lFirst * SECTOR_SIZE,
+                        SEEK_SET)) < 0){
+               perror("fseek failed");
+               return EXIT_FAILURE;
+            }
+
+            if ((res = fread(subpt, sizeof(pt_entry), 4, image)) < 0) {
+               perror("fread failed");
+               return EXIT_FAILURE;
+            }
+
+            /* Validate MINIX subparition */
+            if (subpt[sub].type != MINIX_PART){
+               perror("This is not a MINIX partition\n");
+               return EXIT_FAILURE;
+            }
+
+            /* Get the file system */
+            offset = subpt[sub].lFirst * SECTOR_SIZE;
+         }
+         else{
+            /* Get the file system */
+            if ((res = fseek(image, PART_OFFSET + pt[part].lFirst * SECTOR_SIZE
+                        , SEEK_SET)) < 0){
+               perror("fseek failed");
+               return EXIT_FAILURE;
+            }
          }
       }
+
+      if ((res = fseek(image, SUPERBLOCK_OFFSET + offset, SEEK_SET)) < 0){
+         perror("fseek failed");
+         return EXIT_FAILURE;
+      }
+
+      if ((res = fread(sBlock, sizeof(superblock), 1, image)) < 0){
+         perror("fread failed");
+         return EXIT_FAILURE;
+      }
+
+      if (sBlock->magic != MINIX_MAGIC){
+         printf("Bad magic number. (1x%04x)\n", sBlock->magic);
+         printf("This doesn't look like a MINIX filesystem.\n");
+         return EXIT_FAILURE;
+      }
+
+      /* Find the root inode */
+      if ((res = fseek(image, offset +
+                  (sBlock->i_blocks + sBlock->z_blocks + 2) * sBlock->blocksize,
+                  SEEK_SET)) < 0){
+         perror("fseek failed");
+         return EXIT_FAILURE;
+      }
+
+      if ((res = fread(Inode, sizeof(inode), 1, image)) < 0){
+         perror("fread failed");
+         return EXIT_FAILURE;
+      }
+
+      zonesize = sBlock->blocksize << sBlock->log_zone_size;
+
+      if (strlen(path) == 0){
+         strcpy(path, "/:\0");
+      }
+
+      printFiles(image, sBlock, offset, path, Inode, zonesize);
+
+      if (v){
+         printf("\n");
+         printSuperblock(sBlock);
+         printf("\n");
+         printInode(Inode);
+      }
+
+      free(pt);
+      free(subpt);
+      free(sBlock);
+      free(Inode);
+      fclose(image);
+      return 0;
    }
-
-   if ((res = fseek(image, SUPERBLOCK_OFFSET + offset, SEEK_SET)) < 0){
-      perror("fseek failed");
-      return EXIT_FAILURE;
-   }
-
-   fread(sBlock, sizeof(superblock), 1, image);
-
-   if (sBlock->magic != MINIX_MAGIC){
-      printf("Bad magic number. (1x%04x)\n", sBlock->magic);
-      printf("This doesn't look like a MINIX filesystem.\n");
-      return EXIT_FAILURE;
-   }
-
-   /* Find the root inode */
-   if ((res = fseek(image, offset +
-               (sBlock->i_blocks + sBlock->z_blocks + 2) * sBlock->blocksize,
-               SEEK_SET)) < 0){
-      perror("fseek failed");
-      return EXIT_FAILURE;
-   }
-
-   fread(Inode, sizeof(inode), 1, image);
-   zonesize = sBlock->blocksize << sBlock->log_zone_size;
-
-   if (strlen(path) == 0){
-      strcpy(path, "/:\0");
-   }
-
-   printFiles(image, sBlock, offset, path, Inode, zonesize);
-
-   if (v){
-      printf("\n");
-      printSuperblock(sBlock);
-      printf("\n");
-      printInode(Inode);
-   }
-
-
-   /*
-   if (strlen(path) == 0){
-      strcpy(path, "/:\0");
-   }
-
-   printFiles(image, path, Inode, zonesize);
-   */
-
-
-   free(pt);
-   free(subpt);
-   free(sBlock);
-   free(Inode);
-   fclose(image);
-   return 0;
 }
